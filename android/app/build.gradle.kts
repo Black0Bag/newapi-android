28ca6275b6a9ad3b52368c437e38962f66c2e2b9
黑口袋 <1216300859@qq.com>
2026-08-31 05:35:13 +0000

    v0.4.5 fix root init + login check + stop

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.black0bag.newapi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.4.4"
+        versionCode = 8
+        versionName = "0.4.5"
     }
 
     buildTypes {
