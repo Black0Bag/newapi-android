02f7619c7585fdf99ed7114ba8849ff747be1d42
黑口袋 <1216300859@qq.com>
2026-08-31 07:13:52 +0000

    v0.5.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.black0bag.newapi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.5.1"
+        versionCode = 11
+        versionName = "0.5.2"
     }
 
     buildTypes {
