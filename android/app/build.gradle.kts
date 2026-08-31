4ed0867e811ee0d26eb6b708d9d82dd9fcdb4135
黑口袋 <1216300859@qq.com>
2026-08-31 06:49:08 +0000

    bump v0.5.1 channel fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.black0bag.newapi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.5.0"
+        versionCode = 10
+        versionName = "0.5.1"
     }
 
     buildTypes {
